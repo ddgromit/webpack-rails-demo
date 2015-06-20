5c56f051bebcd4f035715296e7e1cba19bde35a8
Derek Dahmer <derekdahmer@gmail.com>
2015-06-19 23:11:43 -0700

    Disable sprockets

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,12 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
 
 module WebpackRailsDemo
   class Application < Rails::Application
