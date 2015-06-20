d1cd105304381218ac1e234fa4b9a806f01e47cd
Derek Dahmer <derekdahmer@gmail.com>
2015-06-20 13:33:49 -0700

    Production configuration

---
+++ webpack/config.prod.js
@@ -0,0 +1,24 @@
+// Load up the common config so we can add stuff to it.
+var config = require('./config.common');
+var webpack = require('webpack');
+
+// Merge
+config.plugins = config.plugins.concat( [
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.AggressiveMergingPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+]);
+
+console.log("Plugins: ", config.plugins);
+
+module.exports = config;
