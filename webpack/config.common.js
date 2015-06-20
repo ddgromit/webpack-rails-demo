a9410992ca6a09e4b4ab9f0eb00bb25d1493ff98
Derek Dahmer <derekdahmer@gmail.com>
2015-06-20 15:39:46 -0700

    Bootstrap

--- webpack/config.common.js
+++ webpack/config.common.js
@@ -8,15 +8,22 @@ module.exports = {
   },
 
   output: {
-    path: './public',
-    filename: 'bundle.[name].js'
+    path: './public/build',
+    filename: 'bundle.[name].js',
+    publicPath: '/build/'
   },
 
   module: {
     loaders: [
       // Babel is a transpiler that supports ES6 and JSX
       { test: /\.js$/, exclude: /(node_modules)|(bootstrap.*\.js)/, loaders: ['babel-loader']},
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader"}
+      { test: /\.less$/, loader: "style-loader!css-loader!autoprefixer!less-loader"},
+
+      // Bootstrap fonts
+      { test: /\.woff[2]?(\?v=\d+\.\d+\.\d+)?$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url-loader?limit=10000&minetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file-loader" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url-loader?limit=10000&minetype=image/svg+xml" }
     ]
   },
 
